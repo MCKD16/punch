982e540d17c3cd7deed1d831843f78d2726a7331
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-05-30 18:41:14 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,19 +1,30 @@
 import discord
 import os
+import datetime
+
 
 client = discord.Client()
 
 
 @client.event
 async def on_ready():
+  print("login")
+  print(client.user.name)
   print(client.user.id)
-  print("ready")
-  
+  print("------------------")
+  await client.change_presence(game=discord.Game(name='Commands: !help', type=1))
+
 @client.event
 async def on_message(message):
-  if message.content.startswith("!help"):
-    await message.channel.send("성공")
- 
+  SN = "`[스넷봇 시스템]"
+  serverf = "Online"
+  servert = "Online"
+  if message.content == "=":
+    await client.send_message(message.channel, "```정확하지 않은 명령어입니다.```")
+  else:
+    if message.content[1:5] == "help":
+      await client.send_message(message.channel, "Command Help:")
+      await client.send_message(message.channel, "```!help - Punch Bot에 대한 명령어목록을 확인합니다.```")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
