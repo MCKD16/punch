c49ae034e7d9b7c8b363d57534e579d9233012ff
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-05-30 18:03:47 +0900

    Update py.py

--- py.py
+++ py.py
@@ -15,6 +15,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.content.startwith(".help"):
+    if message.channel.is_private and message.author.id != "716206205856382996":
       await client.send_message(message.channel, "Command Help")
       await client.send_message(message.channel, "```!help - Show Punch Bot's Commands\n!download - Sends you the latest version of Punch")
  
