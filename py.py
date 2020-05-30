e75a99a351c419e8687c43905ae2fae83e34a750
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-05-30 18:51:32 +0900

    Update py.py

--- py.py
+++ py.py
@@ -18,10 +18,10 @@ async def on_ready():
 async def on_message(message):
   if message.channel.is_private and message.author.id != "716206205856382996":
     if message.content == "!":
-      await client.send_message(message.author.id, "```정확하지 않은 명령어입니다.```")
+      await client.send_message(message.channel, "```정확하지 않은 명령어입니다.```")
     else:
       if message.content[1:5] == "help":
-        await client.send_message(message.author.id, "Command Help:\n```!help - Punch Bot의 명령어들을 확인합니다.```")
+        await client.send_message(message.channel, "Command Help:\n```!help - Punch Bot의 명령어들을 확인합니다.```")
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
