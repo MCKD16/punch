db8eab704f7f195ca67882b580946348206c95ee
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-05-30 18:09:41 +0900

    Update py.py

--- py.py
+++ py.py
@@ -14,7 +14,7 @@ async def on_ready():
   
 @client.event
 async def on_message(message):
-  if message.content.startwith("!help"):
+  if message.content.startswith("!help"):
     if message.channel.is_private and message.author.id != "716206205856382996":
       await client.send_message(message.author.id, "Commands:")
       await client.send_message(message.author.id, "```!help - Punch Bot에 대한 명령어들을 확인합니다.")
