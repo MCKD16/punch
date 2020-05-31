4dfadb8435b4235a825c80ba8d56bc5025faac5e
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-05-31 19:22:51 +0900

    Update py.py

--- py.py
+++ py.py
@@ -17,14 +17,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.channel.is_private and message.author.id != "716206205856382996":
-    if message.content == "!":
-      await client.send_message(message.channel, "```정확하지 않은 명령어입니다.```")
-    else:
-      if message.content[0:5] == "help":
+      if message.content == "!help":
         await client.send_message(message.channel, "Command Help:\n```!help - Punch Bot의 명령어들을 확인합니다.\n!download - Punch PVP Client를 다운로드 할 수 있는 링크를 지급 받습니다.```")
-      if message.content[0:9] == "download":
+      if message.content == "!download":
         await client.send_message(message.channel, "다운로드 링크: http://www.mediafire.com/file/bxrwd2y870369ql/Punch.zip/file")
-      if message.content[0:7] == "notice":
+      if message.content == "!notice":
         if message.author.id == "623502843558756394":
           if message.content[8:]:
             await client.send_message(discord.utils.get(client.get_all_channels(), id="713705245280305173"), "@everyone\n" + message.content[8:] + "")
@@ -32,7 +29,7 @@ async def on_message(message):
             await client.send_message(message.channel, "```공지사항으로 등록할 메세지를 적어주세요.```")
         else:
           await client.send_message(message.channel, "```당신은 이 명령어를 사용할 권한이 없습니다.```")
-      if message.content[0:7] == "update":
+      if message.content == "!update":
         if message.author.id == "623502843558756394":
           if message.content[8:]:
             await client.send_message(discord.utils.get(client.get_all_channels(), id="713704015384543262"), "@everyone\n```" + message.content[8:] + "```")
@@ -40,7 +37,7 @@ async def on_message(message):
             await client.send_message(message.channel, "```업데이트으로 등록할 메세지를 적어주세요.```")
         else:
           await client.send_message(message.channel, "```당신은 이 명령어를 사용할 권한이 없습니다.```")
-      if message.content[0:4] == "say":
+      if message.content == "!say":
         if message.author.id == "623502843558756394":
           if message.content[8:]:
             await client.send_message(discord.utils.get(client.get_all_channels(), id=message.content[5:23]), "@everyone\n" + message.content[24:] + "")
