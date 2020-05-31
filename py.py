f3cfb251db5d48c991a5a32cebfc56f2f0224910
MCKD16 <66161038+MCKD16@users.noreply.github.com>
2020-05-31 19:52:16 +0900

    Update py.py

--- py.py
+++ py.py
@@ -17,7 +17,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
   if message.channel.is_private and message.author.id != "716206205856382996":
-      if message.content[1:] == "!":
+      if message.content == "!":
         await client.send_message(message.channel, "```정확하지 않은 명령어입니다.")
       else if message.content[1:5] == "help":
         await client.send_message(message.channel, "Command Help:\n```!help - Punch Bot의 명령어들을 확인합니다.\n!download - Punch PVP Client를 다운로드 할 수 있는 링크를 지급 받습니다.```")
